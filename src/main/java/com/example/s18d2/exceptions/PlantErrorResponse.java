fb4f88224f1e2bf9d42d7c273395a6588183e6e3
skaimarvas <skaimarvas@gmail.com>
2024-02-24 22:33:45 +0300

    PlantErrorResponse was created

---
+++ src/main/java/com/example/s18d2/exceptions/PlantErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.s18d2.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@ToString
+@AllArgsConstructor
+public class PlantErrorResponse {
+    private Integer status;
+    private String message;
+    private LocalDateTime dateTime;
+}
