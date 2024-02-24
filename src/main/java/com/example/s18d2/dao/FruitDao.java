bd17d99c2c1199f6297ad894d72e88cfbb6491a5
skaimarvas <skaimarvas@gmail.com>
2024-02-24 21:59:33 +0300

    methods were added

---
+++ src/main/java/com/example/s18d2/dao/FruitDao.java
@@ -0,0 +1,18 @@
+package com.example.s18d2.dao;
+
+import com.example.s18d2.entity.Fruit;
+import com.example.s18d2.entity.FruitType;
+
+import java.util.List;
+
+public interface FruitDao {
+    Fruit save(Fruit fruit);
+    List<Fruit> findAll();
+    Fruit findById(long id);
+    Fruit update(Fruit fruit);
+    Fruit remove(long id);
+    List<Fruit> findByPrice(double price);
+    List<Fruit> findByFruitType(FruitType fruitType);
+
+
+}
