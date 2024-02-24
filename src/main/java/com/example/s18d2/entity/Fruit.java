cf13dd0d9144091d50382bcebb837b4809cd3986
skaimarvas <skaimarvas@gmail.com>
2024-02-24 21:59:16 +0300

    field and annotations were added

--- src/main/java/com/example/s18d2/entity/Fruit.java
+++ src/main/java/com/example/s18d2/entity/Fruit.java
@@ -1,4 +1,18 @@
 package com.example.s18d2.entity;
 
-public class Fruit {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "fruit",schema = "s18d2")
+public class Fruit extends Plant{
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "fruit_type")
+    @NotNull
+    private FruitType fruitType;
 }
