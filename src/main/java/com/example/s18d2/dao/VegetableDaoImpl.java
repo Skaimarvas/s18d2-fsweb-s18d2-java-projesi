f46bc1da903b657727f48b962eab1c9832be80e2
skaimarvas <skaimarvas@gmail.com>
2024-02-24 22:32:35 +0300

    3 methods were updated

--- src/main/java/com/example/s18d2/dao/VegetableDaoImpl.java
+++ src/main/java/com/example/s18d2/dao/VegetableDaoImpl.java
@@ -3,6 +3,7 @@ package com.example.s18d2.dao;
 import com.example.s18d2.entity.Fruit;
 import com.example.s18d2.entity.FruitType;
 import com.example.s18d2.entity.Vegetable;
+import com.example.s18d2.exceptions.PlantException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
@@ -37,7 +38,7 @@ public class VegetableDaoImpl implements VegetableDao {
     public Vegetable findById(long id) {
         Vegetable vegetable = entityManager.find(Vegetable.class, id);
         if(vegetable == null){
-            throw new VegetableException("Vegetable with given id is not exist " + id, HttpStatus.NOT_FOUND);
+            throw new PlantException("Vegetable with given id is not exist " + id, HttpStatus.NOT_FOUND);
 
         }
         return vegetable;
@@ -69,6 +70,27 @@ public class VegetableDaoImpl implements VegetableDao {
         TypedQuery<Vegetable> query =
                 entityManager.createQuery("SELECT v FROM Vegetable v WHERE v.is_grown_on_tree = :grownOnTree ORDER BY v.name DESC", Vegetable.class);
         query.setParameter("grownOnTree", grownOnTree);
+        return query.getResultList();
+    }
+    @Override
+    public List<Vegetable> getByPriceDesc() {
+        TypedQuery<Vegetable> query =
+                entityManager.createQuery("SELECT v FROM Vegetable v ORDER BY v.price DESC", Vegetable.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Vegetable> getByPriceAsc() {
+        TypedQuery<Vegetable> query =
+                entityManager.createQuery("SELECT v FROM Vegetable v ORDER BY v.price ASC", Vegetable.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Vegetable> searchByName(String name) {
+        TypedQuery<Vegetable> query =
+                entityManager.createQuery("SELECT v FROM Vegetable v WHERE LOWER(v.name) LIKE LOWER(:name)", Vegetable.class);
+        query.setParameter("name", "%" + name + "%");
         return query.getResultList();
     }
 }
