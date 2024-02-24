269ad7021a0b463c6d8832903afa9449887483dc
skaimarvas <skaimarvas@gmail.com>
2024-02-24 22:01:48 +0300

    field was added

--- src/main/java/com/example/s18d2/entity/Vegetable.java
+++ src/main/java/com/example/s18d2/entity/Vegetable.java
@@ -1,4 +1,20 @@
 package com.example.s18d2.entity;
 
-public class Vegetable {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "vegetable", schema = "s18d2")
+public class Vegetable extends Plant{
+
+
+
+    @Column(name = "is_grown_on_tree")
+    private boolean isGrownOnTree;
 }
