1d1dcd2e2bdcddc00785ce703c03c5850291f251
skaimarvas <skaimarvas@gmail.com>
2024-02-24 22:33:26 +0300

    methods were updated

--- src/main/java/com/example/s18d2/services/FruitServiceImpl.java
+++ src/main/java/com/example/s18d2/services/FruitServiceImpl.java
@@ -1,4 +1,46 @@
 package com.example.s18d2.services;
 
-public class FruitServiceImpl {
+import com.example.s18d2.dao.FruitDaoImpl;
+import com.example.s18d2.entity.Fruit;
+
+import java.util.List;
+
+public class FruitServiceImpl implements FruitService {
+
+    private final FruitDaoImpl fruitDao;
+
+
+    public FruitServiceImpl(FruitDaoImpl fruitDao){
+        this.fruitDao = fruitDao;
+    }
+
+    @Override
+    public List<Fruit> getByPriceAsc() {
+        return null;
+    }
+
+    @Override
+    public Fruit getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Fruit> getByPriceDesc() {
+        return null;
+    }
+
+    @Override
+    public Fruit save(Fruit fruit) {
+        return null;
+    }
+
+    @Override
+    public Fruit delete(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Fruit> searchByName(String name) {
+        return null;
+    }
 }
