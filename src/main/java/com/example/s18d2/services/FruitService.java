77d400089808955a1601b4874f564d86ab642e01
skaimarvas <skaimarvas@gmail.com>
2024-02-24 22:33:14 +0300

    methods were added

--- src/main/java/com/example/s18d2/services/FruitService.java
+++ src/main/java/com/example/s18d2/services/FruitService.java
@@ -1,4 +1,15 @@
 package com.example.s18d2.services;
 
+import com.example.s18d2.entity.Fruit;
+
+import java.util.List;
+
 public interface FruitService {
+    List<Fruit> getByPriceAsc();
+    Fruit getById(long id);
+    List<Fruit> getByPriceDesc();
+    Fruit save(Fruit fruit);
+    Fruit delete(long id);
+    List<Fruit> searchByName(String name);
+
 }
