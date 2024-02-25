55dee17e439421ede2e0e58591effa34537acbfd
skaimarvas <skaimarvas@gmail.com>
2024-02-25 15:03:14 +0300

    VegetableService methods were added

--- src/main/java/com/example/s18d2/services/VegetableService.java
+++ src/main/java/com/example/s18d2/services/VegetableService.java
@@ -1,4 +1,15 @@
 package com.example.s18d2.services;
 
+import com.example.s18d2.entity.Vegetable;
+
+import java.util.List;
+
 public interface VegetableService {
+    List<Vegetable> findAll();
+    List<Vegetable> getByPriceAsc();
+    Vegetable getById(long id);
+    List<Vegetable> getByPriceDesc();
+    Vegetable save(Vegetable vegetable);
+    Vegetable delete(long id);
+    List<Vegetable> searchByName(String name);
 }
