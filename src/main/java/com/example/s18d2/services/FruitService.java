595cae3edea7abe033572c80bdfb914043654291
skaimarvas <skaimarvas@gmail.com>
2024-02-25 15:01:54 +0300

    findAll method was added

--- src/main/java/com/example/s18d2/services/FruitService.java
+++ src/main/java/com/example/s18d2/services/FruitService.java
@@ -5,6 +5,7 @@ import com.example.s18d2.entity.Fruit;
 import java.util.List;
 
 public interface FruitService {
+    List<Fruit> findAll();
     List<Fruit> getByPriceAsc();
     Fruit getById(long id);
     List<Fruit> getByPriceDesc();
