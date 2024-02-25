aea0a43bc366f7d4e6bb52b90eee48ee2537e015
skaimarvas <skaimarvas@gmail.com>
2024-02-25 15:02:17 +0300

    findAll method and @Service annotation was added

--- src/main/java/com/example/s18d2/services/FruitServiceImpl.java
+++ src/main/java/com/example/s18d2/services/FruitServiceImpl.java
@@ -2,9 +2,14 @@ package com.example.s18d2.services;
 
 import com.example.s18d2.dao.FruitDaoImpl;
 import com.example.s18d2.entity.Fruit;
+import com.example.s18d2.exceptions.PlantException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class FruitServiceImpl implements FruitService {
 
     private final FruitDaoImpl fruitDao;
@@ -14,33 +19,52 @@ public class FruitServiceImpl implements FruitService {
         this.fruitDao = fruitDao;
     }
 
+
+
+    @Override
+    public List<Fruit> findAll(){
+        return  fruitDao.findAll();
+    }
     @Override
     public List<Fruit> getByPriceAsc() {
-        return null;
+        return fruitDao.getByPriceAsc();
     }
 
     @Override
     public Fruit getById(long id) {
-        return null;
+        Optional<Fruit> optionalFruit = Optional.ofNullable(fruitDao.findById(id));
+
+        if(optionalFruit.isPresent()){
+            return optionalFruit.get();
+        }
+        throw new PlantException("plant with given id is not exit " + id, HttpStatus.NOT_FOUND);
     }
 
     @Override
     public List<Fruit> getByPriceDesc() {
-        return null;
+        return fruitDao.getByPriceDesc();
     }
 
     @Override
     public Fruit save(Fruit fruit) {
-        return null;
+        return fruitDao.save(fruit);
     }
 
     @Override
     public Fruit delete(long id) {
+        Fruit foundFruit = getById(id);
+
+        if(foundFruit != null){
+            fruitDao.remove(id);
+            return foundFruit;
+
+        }
+
         return null;
     }
 
     @Override
     public List<Fruit> searchByName(String name) {
-        return null;
+        return fruitDao.searchByName(name);
     }
 }
