b88600b3845aa42feebe0831092864a7bf775551
skaimarvas <skaimarvas@gmail.com>
2024-02-25 15:01:42 +0300

    FruitController was updated.

--- src/main/java/com/example/s18d2/controller/FruitController.java
+++ src/main/java/com/example/s18d2/controller/FruitController.java
@@ -1,4 +1,54 @@
 package com.example.s18d2.controller;
 
+import com.example.s18d2.dto.FruitDto;
+import com.example.s18d2.entity.Fruit;
+import com.example.s18d2.services.FruitService;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fruit")
 public class FruitController {
+    private final FruitService fruitService;
+
+    public FruitController(FruitService fruitService){
+        this.fruitService = fruitService;
+    }
+
+    @GetMapping
+    public List<Fruit> findAll(){
+        return fruitService.findAll();
+    }
+    @GetMapping("/asc")
+    public List<Fruit> get(){
+        return fruitService.getByPriceAsc();
+    }
+
+    @GetMapping("/desc")
+    public List<Fruit> getDesc(){
+        return fruitService.getByPriceDesc();
+    }
+
+    @GetMapping("/{id}")
+    public FruitDto getById(@Positive @PathVariable long id){
+        return new FruitDto("succed", fruitService.getById(id));
+    }
+
+    @PostMapping
+    public Fruit save(@Validated @RequestBody Fruit fruit){
+        return fruitService.save(fruit);
+
+    }
+    @GetMapping("/name/{name}")
+    public List<Fruit> getByName(@Size(min = 2, max = 45) @PathVariable String name){
+        return fruitService.searchByName(name);
+    }
+    @DeleteMapping("/{id}")
+    public Fruit delete(@PathVariable long id){
+        return fruitService.delete(id);
+    }
 }
